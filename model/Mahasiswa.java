8710335438fbce3cf43ef5477bbc54aba6eb9f7d
normansyah12 <dwinormansyah31@gmail.com>
2020-04-17 21:49:50 +0700

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+﻿package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa===========");
+  System.out.println("NPM : 18631045");
+  System.out.println("Nama : Dwi Normansyah");
+  System.out.println("============================");
+ }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+﻿package model;
+
+public class Matakuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mata Kuliah===========");
+  System.out.println("kode : TIF3402");
+  System.out.println("Nama : pemrograman Berbasis Objek 1");
+  System.out.println("========================");
+ }
+}
